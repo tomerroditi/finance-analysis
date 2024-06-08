b78c6e8ca2b1fa70583c098d7676f58ef279a3af
tomer <tomerroditi1@gmail.com>
2024-06-08 21:55:26 +0300

    added javascript script for hapoalim scraping.

---
+++ src/scraper/node/hapoalim.js
@@ -0,0 +1,44 @@
+import { CompanyTypes, createScraper } from 'israeli-bank-scrapers';
+
+// get the id, card6Digits and password from the command line
+const args = process.argv.slice(2);
+const userCode = args[0];
+const password = args[1];
+const start_date = args[2];
+
+
+(async function() {
+  try {
+    // create the options object
+    const options = {
+      companyId: CompanyTypes.hapoalim,
+      startDate: new Date(start_date),
+      combineInstallments: false,
+      showBrowser: false
+    };
+
+    // create the credentials object
+    const credentials = {
+      userCode: userCode,
+      password: password
+    };
+
+    // create the scraper object
+    const scraper = createScraper(options);
+    const scrapeResult = await scraper.scrape(credentials);
+
+    if (scrapeResult.success) {
+      scrapeResult.accounts.forEach((account) => {
+        console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
+        account.txns.forEach((txn) => {
+          console.log(`account number: ${account.accountNumber}| type: ${txn.type}| id: ${txn.identifier}| date: ${txn.date}| amount: ${txn.chargedAmount}| desc: ${txn.description}| status: ${txn.status}`);
+        });
+      });
+    }
+    else {  // if the scraping failed
+      throw new Error(scrapeResult.errorType);
+    }
+  } catch(e) {
+    console.error(`scraping failed for the following reason: ${e.message}`);
+  }
+})();
