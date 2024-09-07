1783c164010cb74ceeceb3b680563f26300d9f98
tomer <tomerroditi1@gmail.com>
2024-09-07 21:18:02 +0300

    feat(onezero): onezero scraper now support fetching a new long term token when the current one is not valid. improved the scraping flow.

--- fad/scraper/node/onezero.js
+++ fad/scraper/node/onezero.js
@@ -1,45 +1,91 @@
 import { CompanyTypes, createScraper } from 'israeli-bank-scrapers';
+import readline from "readline";
 
 // Get the credentials and other parameters from command line arguments
 const args = process.argv.slice(2);
 const email = args[0];
 const password = args[1];
-const otpLongTermToken = args[2];
-const phoneNumber = args[3];
+const phoneNumber = args[2];
+const otpLongTermToken = args[3];
 const start_date = args[4];
 
-(async function() {
-  try {
-    // Define the scraper options
-    const options = {
-      companyId: CompanyTypes.oneZero,  // Adjust based on the specific bank
-      startDate: new Date(start_date),
-      combineInstallments: false,
-      showBrowser: true
-    };
+async function otpCodeRetriever() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-    const scraper = createScraper(options);
+  return new Promise((resolve, reject) => {
+    rl.question('Enter OTP code: ', (otpCode) => {
+      resolve(otpCode);
+      rl.close();
+    });
+  });
+}
+
+
+function scrapeResultsToConsole(scrapeResult) {
+  scrapeResult.accounts.forEach((account) => {
+    console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
+    account.txns.forEach((txn) => {
+      console.log(`account number: ${account.accountNumber}| type: ${txn.type}| id: ${txn.identifier}| date: ${txn.date}| amount: ${txn.chargedAmount}| desc: ${txn.description}| status: ${txn.status}`);
+    });
+  });
+  console.log('finished scraping');
+}
+
+function renewLongTermToken(scraper) {
+  return new Promise(async (resolve, reject) => {
     const credentials = {
       email: email,
       password: password,
       phoneNumber: phoneNumber,
-      otpLongTermToken: otpLongTermToken
-    };
-
-    // Start scraping with the provided credentials
-    const scrapeResult = await scraper.scrape(credentials);
-
-    if (scrapeResult.success) {
-      scrapeResult.accounts.forEach((account) => {
-        console.log(`found ${account.txns.length} transactions for account number ${account.accountNumber}`);
-        account.txns.forEach((txn) => {
-          console.log(`account number: ${account.accountNumber}| type: ${txn.type}| id: ${txn.identifier}| date: ${txn.date}| amount: ${txn.chargedAmount}| desc: ${txn.description}| status: ${txn.status}`);
-        });
-      });
-    } else {
-      throw new Error(scrapeResult.errorType);
+      otpCodeRetriever: otpCodeRetriever
     }
-  } catch (e) {
-    console.error(`Scraping failed for the following reason: ${e.message}`);
+    const otpTokenResult = await scraper.resolveOtpToken(credentials);
+    if (otpTokenResult.success) {
+      resolve(otpTokenResult.longTermTwoFactorAuthToken);
+    } else {
+      reject(new Error(`${otpTokenResult.errorType}: ${otpTokenResult.errorMessage}`));
+    }
+  });
+}
+
+
+try {
+  console.log('starting scraper');
+  // Define the scraper options
+  const options = {
+    companyId: CompanyTypes.oneZero,  // Adjust based on the specific bank
+    startDate: new Date(start_date),
+    combineInstallments: false,
+    showBrowser: true
+  };
+
+  const scraper = createScraper(options);
+  const credentials = {
+    email: email,
+    password: password,
+    phoneNumber: phoneNumber,
+    otpLongTermToken: otpLongTermToken,
+  };
+
+  let scrapeResult
+  scrapeResult = await scraper.scrape(credentials);
+
+  // renew the long term token if needed
+  if (!scrapeResult.success && scrapeResult.errorMessage.includes('reading \'idToken\'')) {
+    credentials.otpLongTermToken = await renewLongTermToken(scraper);
+    console.log('renewed long term token:', credentials.otpLongTermToken);
+    scrapeResult = await scraper.scrape(credentials);
   }
-})();
+
+  if (scrapeResult.success) {
+    scrapeResultsToConsole(scrapeResult);
+  } else {
+    throw new Error(`${scrapeResult.errorType}: ${scrapeResult.errorMessage}`);
+  }
+} catch (e) {
+  console.error(e.message);
+}
+
