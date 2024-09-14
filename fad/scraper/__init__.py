dd8b45eab3d72d4cb80dcb0ffb6b298efa4424fa
tomer <tomerroditi1@gmail.com>
2024-09-14 16:57:48 +0300

    refactor(scraper): added functions and classes to __all__ in the init file

--- fad/scraper/__init__.py
+++ fad/scraper/__init__.py
@@ -5,4 +5,4 @@ NODE_JS_SCRIPTS_DIR = os.path.join(os.path.dirname(__file__), 'node')
 from .two_fa import TwoFAHandler
 from .scrapers import CreditCardScraper, BankScraper, get_scraper, Scraper
 
-__all__ = ['CreditCardScraper', 'BankScraper', 'TwoFAHandler', 'NODE_JS_SCRIPTS_DIR']
+__all__ = ['CreditCardScraper', 'BankScraper', 'TwoFAHandler', 'NODE_JS_SCRIPTS_DIR', 'get_scraper', 'Scraper']
