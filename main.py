d9f142af7851b99802e6c0fcdd8e7223f23e3ebf
tomer <tomerroditi1@gmail.com>
2024-09-14 21:49:48 +0300

    chore(main): deleted redundant file

--- main.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from fad.scraper.credit_card import pull_data
-
-
-if __name__ == "__main__":
-    print('Fetching data from the web...')
-    pull_data(datetime(2024, 1, 1))
