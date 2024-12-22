d50df16c87e013bf7e279ef74f452b125e1601f8
tomer <tomerroditi1@gmail.com>
2024-12-22 22:36:15 +0200

    feature(overview): added an option to select from which scrapers to pull data

--- fad/app/overview.py
+++ fad/app/overview.py
@@ -15,9 +15,28 @@ start_date = st.date_input("Set the date from which to start fetching your data 
 if 'data_pulling_status' not in st.session_state:
     st.session_state.data_pulling_status = {"success": {}, "failed": {}}
 
+creds = load_credentials()
+available_scrapers = []
+for service, providers in creds.items():
+    for provider, accounts in providers.items():
+        for account in accounts:
+            available_scrapers.append(f"{service} - {provider} - {account}")
+# select desired scrapers
+scrapers_to_pull_from = st.pills(
+    "Select the scrapers to pull data from",
+    available_scrapers,
+    selection_mode="multi",
+    default=available_scrapers,
+    key="selected_scrapers_to_pull_from")
+creds_to_use = {}
+for service, providers in creds.items():
+    for provider, accounts in providers.items():
+        for account, cred in accounts.items():
+            if f"{service} - {provider} - {account}" in scrapers_to_pull_from:
+                creds_to_use.update({service: {provider: {account: cred}}})
+
 if st.button("Fetch Data") or st.session_state.get("current_pulled_resources") is not None:  # continue after 2fa scraping
-    creds = load_credentials()
-    pull_data_from_all_scrapers_to_db(start_date, creds, DB_PATH)
+    pull_data_from_all_scrapers_to_db(start_date, creds_to_use, DB_PATH)
     ct = CategoriesAndTags(conn)
     ct.update_raw_data_by_rules()
 
