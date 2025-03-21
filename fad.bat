38b33aefbbc42f3c5aa7f24cc041df479a22dac4
tomer <tomerroditi1@gmail.com>
2025-03-21 13:46:47 +0200

    chore: updated the bash script to pull image from github, and open docker desktop

--- fad.bat
+++ fad.bat
@@ -1,30 +1,53 @@
-#!/bin/bash
-echo "Checking if Docker is installed..."
+@echo off
+echo Checking if Docker is installed...
 
-if ! command -v docker &> /dev/null
-then
-    echo "Docker is not installed. Installing Docker..."
-    curl -fsSL https://get.docker.com | sh
-    sudo usermod -aG docker $USER
-    newgrp docker
-fi
+:: Check if Docker is installed
+docker --version >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker is not installed. Installing Docker now...
+
+    :: Download Docker installer
+    powershell -Command "& {Invoke-WebRequest -Uri 'https://desktop.docker.com/win/main/amd64/Docker Desktop Installer.exe' -OutFile 'DockerInstaller.exe'}"
+
+    echo Installing Docker...
+    start /wait DockerInstaller.exe install
+
+    echo Docker installed successfully. Restart your computer and run this script again.
+    exit /b
+)
+
+:: Check if Docker Daemon is running
+docker info >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Docker is not running. Starting Docker Desktop...
+    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+    echo Waiting for Docker to start...
+
+    :CHECK_DOCKER
+    docker info >nul 2>&1
+    IF %ERRORLEVEL% NEQ 0 (
+        timeout /t 5 >nul
+        goto CHECK_DOCKER
+    )
+    echo Docker is now running!
+)
+
+:: Define user directory for persistent storage
+set APP_DIR=%USERPROFILE%\.finance_analysis
+if not exist %APP_DIR% mkdir %APP_DIR%
 
 echo "Pulling the latest version of the Finance Analysis app..."
 docker pull ghcr.io/tomerroditi/finance-analysis:latest
 
-:: Define user directory for persistent storage
-set APP_DIR=$HOME/.finance_analysis
-if not exist %APP_DIR% mkdir %APP_DIR%
-
-echo "Running the Finance Analysis app..."
-docker run -p 8501:8501 \
-    -v "$HOME/.finance_analysis:/app/finance_analysis/fad/resources" \
-    --name finance_local \
+:: Run the Docker container
+start /B docker run -p 8501:8501 ^
+    -v %APP_DIR%:/app/finance_analysis/fad/resources ^
+    --name finance_analysis ^
     --rm ghcr.io/tomerroditi/finance-analysis:latest
 
-
 :: Wait for a few seconds to allow Streamlit to start
-timeout /t 5 /nobreak >nul
+timeout /t 1 /nobreak >nul
 
 :: Open Streamlit in the default web browser
 start http://localhost:8501/
