5e54bece65e1e7cd769a663949fe4b3b11bd24cc
tomer <tomerroditi1@gmail.com>
2025-10-04 16:33:48 +0300

    refactor(settings): simplify settings UI layout
    removed components that were relocated to other pages.

--- fad/app/pages/settings.py
+++ fad/app/pages/settings.py
@@ -1,37 +1,5 @@
 import streamlit as st
 
-from fad.app.components.credentials_components import CredentialsComponents
-from fad.app.services.credentials_service import CredentialsService
 
-############################################################################################################
-# UI
-############################################################################################################
-st.title('App Settings and Credentials')
-st.write("This page contains all the settings for the app and credentials for banks, credit cards and insurance "
-         "companies. You can edit your credentials here.")
-
-settings_tab, credentials_tab = st.tabs(['Settings', 'Credentials'])
-
-with settings_tab:
-    st.write('Settings')
-    st.write('Coming soon...')
-
-with credentials_tab:
-    # fetch credentials
-    credentials = CredentialsService().credentials
-    credentials_ui = CredentialsComponents()
-
-    # open a tab for each service
-    cards_tab, banks_tab, insurance_tab = st.tabs(['Credit Cards', 'Banks', 'Insurance'])
-
-    with cards_tab:
-        credentials_ui.edit_delete_credentials('credit_cards')
-        credentials_ui.add_new_data_source('credit_cards')
-
-    with banks_tab:
-        credentials_ui.edit_delete_credentials('banks')
-        credentials_ui.add_new_data_source('banks')
-
-    with insurance_tab:
-        credentials_ui.edit_delete_credentials('insurances')
-        credentials_ui.add_new_data_source('insurances')
+st.title('App Settings')
+st.write('Coming soon...')
