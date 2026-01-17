0eacd20bddf5d43a62ede9a55e716708e7ac0c39
tomerroditi <tomerroditi1@gmail.com>
2026-01-17 19:13:34 +0200

    fix: fixed incorrect default value of env variables

--- backend/main.py
+++ backend/main.py
@@ -54,7 +54,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=os.getenv(
         "CORS_ORIGINS",
-        "http://localhost:5173,http://localhost:3000,http://127.0.0.1:5173",
+        "http://localhost:5173,http://127.0.0.1:5173",
     ).split(","),
     allow_credentials=True,
     allow_methods=["*"],
