f8ee846d4eae6dac7d8414a5c0c2517378a3043d
tomerroditi <tomerroditi1@gmail.com>
2026-01-17 15:15:29 +0200

    chore(frontend): add consistent-type-imports eslint rule

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,14 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+    },
   },
 ])
